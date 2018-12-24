f5d839f92fba64bd4cbe57991cc0380ddc5c2f49
Keyti Toromanova <ktoromanova@luc.edu>
2018-12-24 15:50:36 -0600

    Add additional methods for even digits computation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,27 @@ public class Main {
 	// write your code here
     }
 
+    // Get the sum of every second digit from right the left, if it
+    // results in a two-digit number, add up the to digits
+    public static int sumOfDoubleEvenPlace(long number)
+    {
+        int sum = 0;
+        String num = number + "";
+        for (int i = getSize(number) - 2; i >= 0; i -= 2)
+            sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);
+
+        return sum;
+    }
+
+    // Return this number if it is a single digit, otherwise,
+    // return the sum of the two digits
+    public static int getDigit(int number)
+    {
+        if (number < 9)
+            return number;
+        return number / 10 + number % 10;
+    }
+
     // A method to return the numbers of digits
     public static int getSize(long digit)
     {
