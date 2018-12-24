b846d43562ad705c85719b40c34afe16646e138a
Keyti Toromanova <ktoromanova@luc.edu>
2018-12-24 15:37:50 -0600

    Add a method for number of digits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
+
+    // A method to return the numbers of digits
+    public static int getSize(long digit)
+    {
+        String num = digit + "";
+        return num.length();
+    }
+
 }
